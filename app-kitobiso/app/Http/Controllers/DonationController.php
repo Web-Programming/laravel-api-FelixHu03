327224c1ad95bc7507ef522422bd84f6434b192f
Felix <felix_2327250059@mhs.mdp.ac.id>
2024-09-19 19:22:37 +0700

    controller CRUD

--- app-kitobiso/app/Http/Controllers/DonationController.php
+++ app-kitobiso/app/Http/Controllers/DonationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\donation;
+use App\Models\Donation;
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class DonationController extends Controller
 {
@@ -14,10 +16,11 @@ class DonationController extends Controller
     {
         $data = [
             'status' => 'success',
-            'massage'=> 'data Donation Berhasil',
-            'data'=> donation::all(),
+            'message' => 'Data donations berhasil diambil',
+            'data' => Donation::all(),
         ];
         return response()->json($data);
+        // return Donation::all();
     }
 
     /**
@@ -25,7 +28,9 @@ class DonationController extends Controller
      */
     public function create()
     {
-        //
+        $input = request()->all();
+        User::create($input);
+        return $input;
     }
 
     /**
@@ -33,38 +38,70 @@ class DonationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $val = $request->validate([
+            'amount' => 'required',
+            'funding_id' => 'required|exists:users,id',
+            'user_id' => 'required|exists:users,id',
+        ]);
+        $donation = Donation::create($val);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data donation berhasil ditambahkan',
+            'data' => $donation,
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(donation $donation)
+    public function show(Donation $donation)
     {
-        //
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data donation berhasil diambil',
+            'data' => $donation,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(donation $donation)
+    public function edit(Donation $donation)
     {
-        //
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data donation berhasil diedit',
+            'data' => $donation,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, donation $donation)
+    public function update(Request $request, Donation $donation)
     {
-        //
+        $val = $request->validate([
+            'amount' => 'required',
+            'funding_id' => 'required|exists:users,id',
+            'user_id' => 'required|exists:users,id',
+        ]);
+        $donation->update($val);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data donation berhasil diupdate',
+            'data' => $donation,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(donation $donation)
+    public function destroy(Donation $donation)
     {
-        //
+        $donation->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data donation berhasil dihapus',
+        ]);
     }
 }
